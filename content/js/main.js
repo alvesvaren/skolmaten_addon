df9bd47cc370cdd59cfd162d1d76984611a26409
Alve Svarén <alve@hotmail.se>
2021-05-01 12:56:24 +0200

    Fix week number

--- content/js/main.js
+++ content/js/main.js
@@ -1,11 +1,12 @@
 "use strict";
 
 Date.prototype.getWeek = function () {
-    const d = new Date(this);
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    var dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-};
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+}
 
 const weekDays = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
 var schools = [];
